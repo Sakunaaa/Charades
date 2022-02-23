138d1cca4d352a9f2ef3ee38bcb47cd95d356bbe
Sakunaaa <robertdaszkiewicz9@gmail.com>
2022-02-23 17:47:20 +0100

    Add createCard function

---
+++ src/categoryCard.js
@@ -0,0 +1,26 @@
+export function createCard(name, image, text) {
+    const card = document.createElement("section")
+    card.classList.add("card")
+    card.style.backgroundImage = `url(${image})`
+    console.log(image)
+    const cardContent = document.createElement("div")
+    cardContent.classList.add("card-content")
+    const title = document.createElement("h2")
+    const titleText = document.createTextNode(name)
+    title.classList.add("card-title")
+    const cardBody = document.createElement("p")
+    const cardBodyText = document.createTextNode(text)
+    cardBody.classList.add("card-body")
+    const checkbox = document.createElement("input")
+    checkbox.setAttribute("type", "checkbox")
+    checkbox.classList.add("checkbox")
+
+    card.appendChild(cardContent)
+    cardContent.appendChild(title)
+    title.appendChild(titleText)
+    cardContent.appendChild(cardBody)
+    cardBody.appendChild(cardBodyText)
+    cardContent.appendChild(checkbox)
+
+    return card
+} 
--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-fetch("http://localhost:3000/categories")
+import {
+    createCard
+} from './categoryCard'
+
+// 1. Przypisać do zmiennej element, do którego chcemy wrzucić nasze karty
+// 2. Musimy pobrać kategorie do kart
+// 3. Po udanym pobraniu, musimy wykorzystać pobrane dane, aby zapełnić nimi karty (createCard)
+const categoriesElement = document.getElementById("categories")
+
+fetch("http://localhost:3000/categories")    
+.then(response => response.json())
+.then((result) => {
+        const categories = result.data
+        for ( const category of categories) {
+            const card = createCard(category.content, category.cover_src, category.description)
+            categoriesElement.appendChild(card)
+        } 
+}).catch((error) => {
+    console.log(error)
+})
